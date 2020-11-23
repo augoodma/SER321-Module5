60ebe0e51846b8cc08a4dab51fcecb21d1175297
augoodma <augoodma@asu.edu>
2020-11-22 23:21:42 -0500

    task 2

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## SER321-Module5 Activity 1
+
 #Task 1:
+
 1. The main structure of this code is that it distributes the merge sort algorithm across several localhost threads (theoretically over a network) to execute the task of sorting an array of integers.  It begins by breaking the array into halves and passing each half to either a Branch object which will halve it again or a Sorter object which will sort its segment.  Both object types utilize function 'peek' and 'remove' functions to accomplish their tasks.  The advantage of this distributed systems is that, as a recurisive function, merge sort can use a lot of memory.  This system breaks spreads memory consumption across threads.  The disadvantage is that the strengths of merge sort are its speed and run time consistency, which, by distributing over a network, is degraded/subject to network stability.
 
 2. This experiment will run the following array test cases three times each and average:
@@ -8,26 +10,37 @@
 	* Test case 3: array of 1000 random numbers (0-999) (same arry used for each tree configuration below)
 
 	Test case 1 run times:
-	* One Sorter: 52 ms, 48 ms, 51 ms. Average: 50.33 ms
-	* One Branch, Two Sorters: 58 ms, 50 ms, 53 ms. Average: 53.67 ms
-	* Three Branches, Four Sorters: 119 ms, 112 ms, 113 ms. Average: 114.67 ms
+	* One Sorter:                    44ms,  45ms,  46ms. Average: ms
+	* One Branch, Two Sorters:       48ms,  49ms,  51ms. Average: ms
+	* Three Branches, Four Sorters: 112ms, 117ms, 115ms. Average: ms
+	* Gradle command:
+	```
+	gradle Starter -Ptest=1
+	```
 
 	Test case 2 run times:
-	* One Sorter: 46 ms, 45 ms, 69 ms. Average: 53.33 ms
-	* One Branch, Two Sorters: 51 ms, 70 ms, 70 ms. Average: 63.67 ms
-	* Three Branches, Four Sorters: 108 ms, 107 ms, 139 ms. Average: 118 ms
+	* One Sorter:                    51ms,  59ms,  45ms. Average: ms
+	* One Branch, Two Sorters:       52ms,  73ms,  51ms. Average: ms
+	* Three Branches, Four Sorters: 117ms, 153ms, 115ms. Average: ms
+	* Gradle command:
+	```
+	gradle Starter -Ptest=2
+	```
 
 	Test case 3 run times:
-	* One Sorter: 865 ms, 625 ms, 621 ms. Average: 703.67 ms
-	* One Branch, Two Sorters: 1890 ms, 1248 ms, 1394 ms. Average: 1510.67 ms
-	* Three Branches, Four Sorters: 3495 ms, 3440 ms, 3486 ms. Average: 3473.67 ms
+	* One Sorter:                    654ms,  667ms,  653ms. Average: ms
+	* One Branch, Two Sorters:      1309ms, 1393ms, 1422ms. Average: ms
+	* Three Branches, Four Sorters: 3442ms, 3467ms, 3576ms. Average: ms
+	* Gradle command:
+	```
+	gradle Starter -Ptest=3 -Psize=1000
+	```
 
 	Analysis:
 	* Test case 1 and 2 are analyzed together to demonstrate unsorted vs sorted distributed merge sort.  Across the board, the sorted array was slightly less performant than the unsorted array which demonstrates that distribution of merge sort does not solve the traditional merge sort problem of not executing a sorted array faster than an unsorted array.  A larger sample size would likely show average times that are similar for sorted and unsorted.
-
 	* Test case 3 demonstrates distributed unsorted merge sort at a large scale.  This test demonstrated that the advantage of merge sort's speed is quickly lost the more complex the tree structure becomes.  A single sorter handled this task signifcantly faster than multiple sorters.  In fact, inefficiently seem to scale with an increasing number of sorters.  Test cases 1 and 2 demonstrate this as well but not as dramatically.
 
-	3. This experiment will run the following tree test cases three times each and average:
+3. This experiment will run the following tree test cases three times each and average:
 	* Test case 4: extended tree
 	            0
           1           2
@@ -44,10 +57,22 @@
      13 14
 
     Test case 4 run times:
-    * Random data: 8586 ms, 8641 ms, 8618 ms. Average:
-    * Static data: 8466 ms, 8351 ms, 8739 ms. Average:
+    * Random data: 5090 ms, 4779 ms, 7985 ms. Average: 7928 ms
+    * Static data: 4269 ms, 4090 ms, 8027 ms. Average: 7954.67 ms
+    ```
+	gradle Starter -Ptest=4 -Psize=500
+	```
 
     Test case 5 run times:
-    * Random data: 8992 ms, 8947 ms, 8911 ms. Average:
-    * Static data: 9103 ms,  ms,  ms. Average:
+    * Random data: 8649 ms, 8407 ms, 8972 ms. Average: 8676 ms
+    * Static data: 8200 ms, 8318 ms, 8436 ms. Average: 8318 ms
 
+    Analysis:
+    * Test 4 with one additional level of depth over test case 3 took, on average, 228.2% the time test case 3 needed for a random array of the same size. Test case 5 with twice the depth of test case 3, but with the same number of Branches an Sorters, took, on average, 249.8% the time test case 3 needed for a random array of the same size.  It appears that inceasing the overall number of nodes, regardless of type, has an extreme negative effect on performance for distributed merge sort.  Likewise, increasing the depth of the array, also causes (although not as dramatically) a degradation in performance.
+    * Whether the data was randomly generated or not seems to be irrelevant.  In test 4 case, random arrays performed slightly better than a static array.  In test 5, the opposite was true.
+
+4. Looking at Wireshark, there is quite a lot of traffic.  It appears that for each node, for passing data down the tree, at each level, there is a three-way handshake, followed by the PSH of data from one node to the next.  As the function stack reduces, Wireshark shows a FIN and ACK.  Being a recursive algorithm, the amount of traffic grows exponentially with the number of levels.
+
+# Task 2:
+
+4. 
--- src/main/java/mergeSort/MergeSort.java
+++ src/main/java/mergeSort/MergeSort.java
@@ -12,6 +12,10 @@ public class MergeSort {
    */
 
   public static int message_id = 0;
+  public static long start;
+  public static long middle;
+  public static long middle2;
+  public static long finish;
   public static JSONObject init(int[] array) {
     JSONArray arr = new JSONArray();
     for (var i : array) {
@@ -35,23 +39,27 @@ public class MergeSort {
     return req;
   }
   
-  public static void Test(int port, String host, int size) {
-    //Uncomment the test case to run
+  public static void Test(int port, String host, int size, int test) {
+    int[] a = null, b = null;
     //Test case 1 (provided array)
-    //int[] a = { 5, 1, 6, 2, 3, 4, 10, 634, 34, 23, 653, 23, 2, 6 };
+    if (test == 1) a = new int[]{5, 1, 6, 2, 3, 4, 10, 634, 34, 23, 653, 23, 2, 6};
 
     //Test case 2 (sorted array)
-    //int[] a =   { 1, 2, 2, 3, 4, 5, 6, 6, 10, 23, 23, 34, 634, 653 };
+    if (test == 2) a = new int[]{1, 2, 2, 3, 4, 5, 6, 6, 10, 23, 23, 34, 634, 653};
 
-    //Test case 3 - 5 (long random array)
-    //int[] a = new int[arraySize];
-    //for(int i = 0; i < a.length; i++)  a[i] = (int) (Math.random() * 999);
+    //Test case 3 - 6 (long random array)
+    if (test >= 3 && test < 7) {
+      a = new int[size];
+      for (int i = 0; i < a.length; i++) a[i] = (int) (Math.random() * 999);
+    }
 
-    //Test case 4 - 5 (long static array)
-    int[] a = RandomArray.get(size);
+    //Test case 4 - 6 (long static array)
+    if (test >= 4 && test < 7) b = RandomArray.get(size);
 
+    start = System.currentTimeMillis();
+    assert a != null;
     JSONObject response = NetworkUtils.send(port, init(a), host);
-    
+
     System.out.println(response);
     response = NetworkUtils.send(port, peek(), host);
     System.out.println(response);
@@ -59,11 +67,34 @@ public class MergeSort {
     while (true) {
       response = NetworkUtils.send(port, remove(), host);
 
+      assert response != null;
       if (response.getBoolean("hasValue")) {
-        System.out.println(response);;
- 
-      } else{
+        System.out.println(response);
+      } else {
+        middle = System.currentTimeMillis();
         break;
+      }
+    }
+
+    if (test >= 4) {
+      middle2 = System.currentTimeMillis();
+      assert b != null;
+      JSONObject response2 = NetworkUtils.send(port, init(b), host);
+
+      System.out.println(response2);
+      response2 = NetworkUtils.send(port, peek(), host);
+      System.out.println(response2);
+
+      while (true) {
+        response2 = NetworkUtils.send(port, remove(), host);
+
+        assert response2 != null;
+        if (response2.getBoolean("hasValue")) {
+          System.out.println(response2);
+        } else {
+          finish = System.currentTimeMillis();
+          break;
+        }
       }
     }
   }
@@ -71,126 +102,134 @@ public class MergeSort {
   public static void main(String[] args) {
     String host = args[0];
     int size = Integer.parseInt(args[1]);
+    int test = Integer.parseInt(args[2]);
+
     // all the listening ports in the setup
     ArrayList<Integer> ports = new ArrayList<>(Arrays.asList(8000, 8001, 8002, 8003, 8004, 8005, 8006, 8007, 8008, 8009,
             8010, 8011, 8012, 8013, 8014));
 
-    // uncomment the tree setup and test functions wanted
     // use this tree setup for test cases 1 - 3
     //      0
     //   1     2
     // 3   4 5   6
-    /*
-    new Thread(new Branch(ports.get(0), ports.get(1), ports.get(2))).start();
-    
-    new Thread(new Branch(ports.get(1), ports.get(3), ports.get(4))).start();
-    new Thread(new Sorter(ports.get(3))).start();
-    new Thread(new Sorter(ports.get(4))).start();
-    
-    new Thread(new Branch(ports.get(2), ports.get(5), ports.get(6))).start();
-    new Thread(new Sorter(ports.get(5))).start();
-    new Thread(new Sorter(ports.get(6))).start();
+    if(test >= 1 && test < 4) {
+      new Thread(new Branch(ports.get(0), ports.get(1), ports.get(2), host)).start();
 
-    // make sure we didn't hang
-    System.out.println("started");
-    long start = System.currentTimeMillis();
-    // One Sorter
-    Test(ports.get(3), host);
-    long finish = System.currentTimeMillis();
-    System.out.println("Test 1 Time: " + (finish - start));
-    System.out.println("Host: " + args[0]);
+      new Thread(new Branch(ports.get(1), ports.get(3), ports.get(4), host)).start();
+      new Thread(new Sorter(ports.get(3))).start();
+      new Thread(new Sorter(ports.get(4))).start();
 
-    start = System.currentTimeMillis();
-    // One branch / Two Sorters
-    Test(ports.get(2), host);
-    finish = System.currentTimeMillis();
-    System.out.println("Test 2 Time: " + (finish - start));
-    System.out.println("Host: " + args[0]);
+      new Thread(new Branch(ports.get(2), ports.get(5), ports.get(6), host)).start();
+      new Thread(new Sorter(ports.get(5))).start();
+      new Thread(new Sorter(ports.get(6))).start();
 
-    start = System.currentTimeMillis();
-    // Three Branch / Four Sorters
-    Test(ports.get(0), host);
-    finish = System.currentTimeMillis();
-    System.out.println("Test 3 Time: " + (finish - start));
-    System.out.println("Host: " + args[0]);
-    */
+      // make sure we didn't hang
+      System.out.println("started");
 
-    // use this node setup for test case 4
-    //            0
-    //      1           2
-    //   3     4     5     6
-    // 7   8 9  10 11 12 13 14
+      // One Sorter
+      Test(ports.get(3), host, size, test);
+      long time1 = middle - start;
 
-    new Thread(new Branch(ports.get(0), ports.get(1), ports.get(2), host)).start();
+      // One branch / Two Sorters
+      Test(ports.get(2), host, size, test);
+      long time2 = middle - start;
 
-    new Thread(new Branch(ports.get(1), ports.get(3), ports.get(4), host)).start();
-    new Thread(new Branch(ports.get(3), ports.get(7), ports.get(8), host)).start();
-    new Thread(new Sorter(ports.get(7))).start();
-    new Thread(new Sorter(ports.get(8))).start();
+      // Three Branch / Four Sorters
+      Test(ports.get(0), host, size, test);
+      long time3 = middle - start;
 
-    new Thread(new Branch(ports.get(4), ports.get(9), ports.get(10), host)).start();
-    new Thread(new Sorter(ports.get(9))).start();
-    new Thread(new Sorter(ports.get(10))).start();
+      System.out.println("One Sorter Time:                  " + time1);
+      System.out.println("One branch / Two Sorters Time:    " + time2);
+      System.out.println("Three Branch / Four Sorters Time: " + time3);
+      System.out.println("Host: " + args[0]);
+    }
+    if(test == 4) {
+      // use this node setup for test case 4
+      //            0
+      //      1           2
+      //   3     4     5     6
+      // 7   8 9  10 11 12 13 14
+      new Thread(new Branch(ports.get(0), ports.get(1), ports.get(2), host)).start();
 
-    new Thread(new Branch(ports.get(2), ports.get(5), ports.get(6), host)).start();
-    new Thread(new Branch(ports.get(5), ports.get(11), ports.get(12), host)).start();
-    new Thread(new Sorter(ports.get(11))).start();
-    new Thread(new Sorter(ports.get(12))).start();
+      new Thread(new Branch(ports.get(1), ports.get(3), ports.get(4), host)).start();
+      new Thread(new Branch(ports.get(3), ports.get(7), ports.get(8), host)).start();
+      new Thread(new Sorter(ports.get(7))).start();
+      new Thread(new Sorter(ports.get(8))).start();
 
-    new Thread(new Branch(ports.get(6), ports.get(13), ports.get(14), host)).start();
-    new Thread(new Sorter(ports.get(13))).start();
-    new Thread(new Sorter(ports.get(14))).start();
+      new Thread(new Branch(ports.get(4), ports.get(9), ports.get(10), host)).start();
+      new Thread(new Sorter(ports.get(9))).start();
+      new Thread(new Sorter(ports.get(10))).start();
 
-    // make sure we didn't hang
-    System.out.println("started");
-    long start = System.currentTimeMillis();
-    // One Sorter
-    Test(ports.get(0), host, size);
-    long finish = System.currentTimeMillis();
-    System.out.println("Test 4 Time: " + (finish - start));
-    System.out.println("Host: " + args[0]);
+      new Thread(new Branch(ports.get(2), ports.get(5), ports.get(6), host)).start();
+      new Thread(new Branch(ports.get(5), ports.get(11), ports.get(12), host)).start();
+      new Thread(new Sorter(ports.get(11))).start();
+      new Thread(new Sorter(ports.get(12))).start();
 
-    // use this node setup for test case 5
-    //           0
-    //          1 2
-    //         3 4
-    //        5 6
-    //       7 8
-    //     9 10
-    //   11 12
-    // 13 14
-/*
-    new Thread(new Branch(ports.get(0), ports.get(1), ports.get(2))).start();
+      new Thread(new Branch(ports.get(6), ports.get(13), ports.get(14), host)).start();
+      new Thread(new Sorter(ports.get(13))).start();
+      new Thread(new Sorter(ports.get(14))).start();
 
-    new Thread(new Branch(ports.get(1), ports.get(3), ports.get(4))).start();
-    new Thread(new Sorter(ports.get(2))).start();
+      // make sure we didn't hang
+      System.out.println("started");
+      // One Sorter
+      Test(ports.get(0), host, size, test);
+      System.out.println("Test 4 Random Array Time: " + (middle - start));
+      System.out.println("Test 4 Static Array Time: " + (finish - middle2));
+      System.out.println("Host: " + args[0]);
+    }
+    if(test == 5) {
+      // use this node setup for test case 5
+      //           0
+      //          1 2
+      //         3 4
+      //        5 6
+      //       7 8
+      //     9 10
+      //   11 12
+      // 13 14
+      new Thread(new Branch(ports.get(0), ports.get(1), ports.get(2), host)).start();
 
-    new Thread(new Branch(ports.get(3), ports.get(5), ports.get(6))).start();
-    new Thread(new Sorter(ports.get(4))).start();
+      new Thread(new Branch(ports.get(1), ports.get(3), ports.get(4), host)).start();
+      new Thread(new Sorter(ports.get(2))).start();
 
-    new Thread(new Branch(ports.get(5), ports.get(7), ports.get(8))).start();
-    new Thread(new Sorter(ports.get(6))).start();
+      new Thread(new Branch(ports.get(3), ports.get(5), ports.get(6), host)).start();
+      new Thread(new Sorter(ports.get(4))).start();
 
-    new Thread(new Branch(ports.get(7), ports.get(9), ports.get(10))).start();
-    new Thread(new Sorter(ports.get(8))).start();
+      new Thread(new Branch(ports.get(5), ports.get(7), ports.get(8), host)).start();
+      new Thread(new Sorter(ports.get(6))).start();
 
-    new Thread(new Branch(ports.get(9), ports.get(11), ports.get(12))).start();
-    new Thread(new Sorter(ports.get(10))).start();
+      new Thread(new Branch(ports.get(7), ports.get(9), ports.get(10), host)).start();
+      new Thread(new Sorter(ports.get(8))).start();
 
-    new Thread(new Branch(ports.get(11), ports.get(13), ports.get(14))).start();
-    new Thread(new Sorter(ports.get(12))).start();
+      new Thread(new Branch(ports.get(9), ports.get(11), ports.get(12), host)).start();
+      new Thread(new Sorter(ports.get(10))).start();
 
-    new Thread(new Sorter(ports.get(13))).start();
-    new Thread(new Sorter(ports.get(14))).start();
+      new Thread(new Branch(ports.get(11), ports.get(13), ports.get(14), host)).start();
+      new Thread(new Sorter(ports.get(12))).start();
 
-    // make sure we didn't hang
-    System.out.println("started");
-    long start = System.currentTimeMillis();
-    // One Sorter
-    Test(ports.get(0), host);
-    long finish = System.currentTimeMillis();
-    System.out.println("Test 5 Time: " + (finish - start));
-    System.out.println("Host: " + args[0]);
-*/
+      new Thread(new Sorter(ports.get(13))).start();
+      new Thread(new Sorter(ports.get(14))).start();
+
+      // make sure we didn't hang
+      System.out.println("started");
+      // One Sorter
+      Test(ports.get(0), host, size, test);
+      System.out.println("Test 5 Random Array Time: " + (middle - start));
+      System.out.println("Test 5 Static Array Time: " + (finish - middle2));
+      System.out.println("Host: " + args[0]);
+    }
+    if(test == 6) {
+      // single node setup for task 2
+      // 0
+      new Thread(new Sorter(ports.get(0))).start();
+
+      // make sure we didn't hang
+      System.out.println("started");
+      // One Sorter
+      Test(ports.get(0), host, size, test);
+      System.out.println("Test 6 Random Array Time: " + (middle - start));
+      System.out.println("Test 6 Static Array Time: " + (finish - middle2));
+      System.out.println("Host: " + args[0]);
+    }
   }
 }
